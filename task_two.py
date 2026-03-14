4baec51a79e6409fa222bff4e3fb41626eea633d
jdeepankur <jdeepankur@outlook.com>
2026-03-14 02:48:41 +0000

    html based response instead of raw JSON

--- task_two.py
+++ task_two.py
@@ -5,15 +5,23 @@ def fetchCustomer(id):
     customer = Database.getRow("customers", "UniqueID", id)
 
     if customer:
-        return {
-            "UniqueID": customer[0],
-            "First_Name": customer[1],
-            "Last_Name": customer[2],
-            "Email": customer[3],
-            "status": customer[4]
-        }
+        html = f"""
+        <html>
+        <body>
+        <h2>Customer Details</h2>
+        <table border="1">
+        <tr><th>UniqueID</th><th>{customer[0]}</th></tr>
+        <tr><td>First Name</td><td>{customer[1]}</td></tr>
+        <tr><td>Last Name</td><td>{customer[2]}</td></tr>
+        <tr><td>Email</td><td>{customer[3]}</td></tr>
+        <tr><td>Status</td><td>{customer[4]}</td></tr>
+        </table>
+        </body>
+        </html>
+        """
+        return html
     else:
-        return {"error": "Customer not found"}
+        return "<html><body><h2>Error: Customer not found</h2></body></html>"
 
 api.new_endpoint("/customer/<id>", fetchCustomer)
 api.start()
