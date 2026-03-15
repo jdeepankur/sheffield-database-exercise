450dd8afac8db60976f54dad573551ddbf7fc532
jdeepankur <jdeepankur@outlook.com>
2026-03-15 03:50:35 +0000

    fix total price decimal points

--- task_three.py
+++ task_three.py
@@ -18,10 +18,12 @@ def fetchCustomersByStatus(status):
     customers = fetchCustomersByField("Status", status)
     return customers
 
-def addNewField(customers, headers, new_field_name, fields_to_combine, operation):
+def addNewField(customers, headers, new_field_name, fields_to_combine, operation, format=None):
     if new_field_name not in headers: headers.append(new_field_name)
     for i, customer in enumerate(customers):
         new_field_value = operation(*[customer[headers.index(field)] for field in fields_to_combine])
+        if format:
+            new_field_value = f'{new_field_value:{format}}'
         customers[i] = customer + (new_field_value,)
 
 customer_headers = Database.getHeaders("customers")
@@ -35,9 +37,9 @@ number_fields = ["UnitPrice", "Quantity"]
 for customer, orders in customer_orders.items():
     for i in range(len(orders)):
         for field in number_fields:
-            orders[i] = orders[i][:order_headers.index(field)] + (float(orders[i][order_headers.index(field)]),) + orders[i][order_headers.index(field)+1:]
+            orders[i] = orders[i][:order_headers.index(field)] + (eval(orders[i][order_headers.index(field)]),) + orders[i][order_headers.index(field)+1:]
 
-    addNewField(orders, order_headers, "Total Price", number_fields, mul)
+    addNewField(orders, order_headers, "Total Price", number_fields, mul, format=".2f")
 
 if not os.path.exists(env("OUTPUT_DIR")):
     os.makedirs(env("OUTPUT_DIR"))
